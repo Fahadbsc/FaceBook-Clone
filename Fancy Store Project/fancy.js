45b6a2393cae583264f49e1b3509e421de13df95
Fahadbsc <159418444+Fahadbsc@users.noreply.github.com>
2024-02-08 19:43:17 +0530

    Delete Fancy Store Project directory

--- Fancy Store Project/fancy.js
+++
@@ -1,5 +0,0 @@
-const btn = document.getElementsByClassName('btn')[0];
-const navbar = document.getElementsByClassName('nav-bar')[0];
-btn.addEventListener('click',()=>{
-    navbar.classList.toggle('mobile');
-})
